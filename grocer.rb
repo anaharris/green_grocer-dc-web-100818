ecdde24fe8df64048c8472f1c99a8d8d97ecff83
Ana Harris <vana1990@gmail.com>
2018-10-07 00:43:45 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,71 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  h = Hash.new(0)
+  sorted = {}
+  cart.each do |item|
+    h[item] += 1
+  end
+  h.each do |hash, num|
+    hash.each do |item, info|
+      sorted[item] = info
+      sorted[item][:count] = num
+    end
+  end
+  return sorted
 end
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |item, info|
+    new_cart[item] = info
+    coupons.each do |el|
+      if item == el[:item]
+        new_cart[item] = new_cart[item] || {}
+        new_cart[item][:count] = cart[item][:count] - el[:num]
+        new_cart["#{item} W/COUPON"] = new_cart["#{item} W/COUPON"] || {}
+        new_cart["#{item} W/COUPON"][:price] = el[:cost]
+        new_cart["#{item} W/COUPON"][:clearance] = info[:clearance]
+        if new_cart["#{item} W/COUPON"][:count] != nil
+          new_cart["#{item} W/COUPON"][:count] += 1
+        else 
+          new_cart["#{item} W/COUPON"][:count] = 1
+        end
+      end
+    end  
+  end
+  return new_cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  new_cart = {}
+  cart.each do |item, info|
+    new_cart[item] = info
+    if info[:clearance] == true
+      new_cart[item]=new_cart[item] || info
+      new_cart[item][:price] = info[:price] - info[:price]/100*20
+    end
+  end
+  return new_cart
 end
 
+
 def checkout(cart, coupons)
-  # code here
+  total = 0
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  binding.pry
+  cart.each do |item, info|
+    if info[:count] > 0 
+      total += info[:price]
+    end
+  end
+  
+  if total > 100
+    total = total - total/100*10
+  end
+  return total
 end
